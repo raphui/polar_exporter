3c163303218c7e60a23e4b8b18f06bb455f41032
Raphael Poggi <poggi.raph@gmail.com>
2016-11-08 22:08:09 +0100

    add export_activity

--- extract.py
+++ extract.py
@@ -5,6 +5,9 @@ import re
 import requests
 import bs4 as BeautifulSoup
 
+sports_flow = {"Swimming": 23, "Running": 1, "Motocross": 54, "Renf Muscu": 34, "Cycling": 2}
+sports_ppt = {"Swimming": 10650934, "Running": 10650925, "Motocross": 10658605, "Renf Muscu": 10731409, "Cycling": 10650928}
+
 def url_post(url, post):
 	r = session.post(url, post)
 	return r
@@ -28,6 +31,21 @@ def login(email, password):
 	reply = url_post(url, post)
 	print(reply.status_code)
 
+def export_activity(sport, id):
+	url = "https://www.polarpersonaltrainer.com/user/calendar/item/multisportExercise_ajaxTransferExerciseToFlow.xml"
+	headers = {
+		'X-Requested-With': 'XMLHttpRequest',
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+		'Content-Type': 'text/xml',
+		'Referer': 'https://www.polarpersonaltrainer.com/user/calendar/item/analyze.ftl?id=' + str(id)
+	}
+	request = '<request><object name="root"><prop name="exerciseId" type="string"><![CDATA[' + str(id) + ']]></prop><object name="sportMapping"><prop name="sportIdInPPT" type="string"><![CDATA[' + str(sports_ppt[sport]) + ']]></prop><prop name="sportIdInFlow" type="string"><![CDATA[' + str(sports_flow[sport]) + ']]></prop></object></object></request>'
+
+	reply = session.post(url, request, headers=headers)
+
+	print(reply.status_code)
+
+
 def parse_activities(html):
 	soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
 	inputs = soup.find_all('input', attrs={"name": "calendarItemName"})
@@ -49,7 +67,8 @@ def parse_activities(html):
 
 	print("Found " + str(n) + " activities links")
 
-		print(l['href'])
+	for i in range(n):
+		export_activity(inputs[i]['value'], links[i]['href'].partition("id=")[2])
 
 def main(argv):
 	global session
